7de00b1d5eb6b0d7a1dd02fa524e7e9a20b7a82b
SA9RAEL <sa9rael@yandex.ru>
2025-01-06 13:10:07 +0300

    The account creation and sign up screens are working. I'm making a new teacher card screen with the option to sign up for a lesson.

--- app/src/main/java/com/example/vocallessons/SignUpUserActivity.kt
+++ app/src/main/java/com/example/vocallessons/SignUpUserActivity.kt
@@ -1,5 +1,30 @@
 package com.example.vocallessons
 
-class SignUpUserActivity {
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.example.vocallessons.databinding.ActivitySignUpUserBinding
 
+class SignUpUserActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignUpUserBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivitySignUpUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+            goLoginUserPage.setOnClickListener {
+                startActivity(Intent(this@SignUpUserActivity, LoginUserActivity::class.java))
+            }
+            createAccountBtn.setOnClickListener {
+                startActivity(Intent(this@SignUpUserActivity, MainActivity::class.java))
+                finish()
+            }
+
+        }
+    }
 }
