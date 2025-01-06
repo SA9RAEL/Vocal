1e715ace98f685e9b9285163b0565d29bbcdd855
SA9RAEL <sa9rael@yandex.ru>
2025-01-06 16:17:32 +0300

    Final preparations. Version 0.1.

--- app/src/main/java/com/example/vocallessons/ConfirmationActivity.kt
+++ app/src/main/java/com/example/vocallessons/ConfirmationActivity.kt
@@ -1,6 +1,7 @@
 package com.example.vocallessons
 
 import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.content.Intent
 import android.icu.text.SimpleDateFormat
 import android.icu.util.Calendar
@@ -23,31 +24,49 @@ class ConfirmationActivity : AppCompatActivity() {
 
         with(binding) {
             selectDateBtn.setOnClickListener {
-                showDAtePicker()
+                showDatePicker()
             }
             confirmButton.setOnClickListener {
                 startActivity(Intent(this@ConfirmationActivity, CongratsActivity::class.java))
+            }
+            selectTimeBtn.setOnClickListener {
+                showTimePicker()
             }
         }
 
 
     }
 
-    private fun showDAtePicker() {
+    private fun showTimePicker() {
+        val timeSetListener = TimePickerDialog.OnTimeSetListener { timePicker, hour, minute ->
+            calendar.set(Calendar.HOUR_OF_DAY, hour)
+            calendar.set(Calendar.MINUTE, minute)
+            binding.userTimeTv.text = "Selected time: " +
+                    SimpleDateFormat("HH:mm", Locale.getDefault()).format(calendar.time)
+        }
+        TimePickerDialog(
+            this,
+            timeSetListener,
+            calendar.get(Calendar.HOUR_OF_DAY),
+            calendar.get(Calendar.MINUTE),
+            true
+        ).show()
+    }
+
+    private fun showDatePicker() {
         val datePickerDialog =
             DatePickerDialog(
-                this, { DatePicker, year: Int, monthOfYear: Int, dayOfMonth: Int ->
+                this, { _, year: Int, monthOfYear: Int, dayOfMonth: Int ->
                     val selectedDate = Calendar.getInstance()
                     selectedDate.set(year, monthOfYear, dayOfMonth)
                     val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
                     val formattedDate = dateFormat.format(selectedDate.time)
-                    binding.userDateTv.text = "Selected Date:$formattedDate"
+                    binding.userDateTv.text = "Selected Date: $formattedDate"
                 },
 
                 calendar.get(Calendar.YEAR),
                 calendar.get(Calendar.MONTH),
                 calendar.get(Calendar.DAY_OF_MONTH)
-
             )
         datePickerDialog.show()
     }
